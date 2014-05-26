720c44bf9f20f6ef36ae77e0a374b38f7ed51435
Vincent <awende.cool@163.com>
2014-05-26 21:38:36 +0800

    JDK7的新特性直接操作二进制和_下划线

---
+++ src/cn/nsu/edu/base/BinaryType.java
@@ -0,0 +1,9 @@
+public class  BinaryType {
+	public static void main(String[] args) {
+		//×¢ÊÍµÄÎÊÌâ
+		 int a = 0b0000_0000_0000_0000_0000_0000_0000_0011;
+		 int b = 0b00000000000000000000000000000011;
+		 System.out.println(a);
+		 System.out.println(b);
+	}
+}
