ae3dac9097bad81b1146aaba294795603ac99520
Vincent <awende.cool@163.com>
2014-05-27 10:21:35 +0800

    求100奇数的和

--- src/cn/nsu/edu/base/ForSum.java
+++ src/cn/nsu/edu/base/ForSum.java
@@ -6,17 +6,20 @@ package cn.nsu.edu.base;
 /**
  * 
  * @author Vincent
- *
+ * 
  */
 public class ForSum {
-	/*
-	 * 利用for求100的和
-	 */
-	public static void main(String [] args){
-		int sum = 0;
-		for(int i=0;i<=100;i++){
-			sum+=i;
+
+	// 利用for求100的和
+
+	public static void main(String[] args) {
+		int oddsum = 0;// 保存奇数
+		for (int i = 0; i <= 100; i++) {
+			
+			if (i % 2 != 0) {
+				oddsum += i;
+			}
 		}
-		System.out.println(sum);
+		System.out.println("奇数的和："+oddsum);
 	}
 }
