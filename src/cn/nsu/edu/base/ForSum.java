ffaa86f5e5036e608870fa250b0db53a2a081641
Vincent <awende.cool@163.com>
2014-05-27 10:16:29 +0800

    利用for循环求100的和

---
+++ src/cn/nsu/edu/base/ForSum.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package cn.nsu.edu.base;
+
+/**
+ * 
+ * @author Vincent
+ *
+ */
+public class ForSum {
+	/*
+	 * 利用for求100的和
+	 */
+	public static void main(String [] args){
+		int sum = 0;
+		for(int i=0;i<=100;i++){
+			sum+=i;
+		}
+		System.out.println(sum);
+	}
+}
