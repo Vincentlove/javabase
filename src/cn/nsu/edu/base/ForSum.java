475f984c43afabdf58d7d2da3f996050dedac551
Vincent <awende.cool@163.com>
2014-05-27 10:31:12 +0800

    100以内偶数的和

--- src/cn/nsu/edu/base/ForSum.java
+++ src/cn/nsu/edu/base/ForSum.java
@@ -14,12 +14,18 @@ public class ForSum {
 
 	public static void main(String[] args) {
 		int oddsum = 0;// 保存奇数
+		int evensum = 0;//保存偶数
 		for (int i = 0; i <= 100; i++) {
 			
 			if (i % 2 != 0) {
 				oddsum += i;
+			}else{
+				evensum+= i;
 			}
 		}
+		int sum= oddsum+evensum;
 		System.out.println("奇数的和："+oddsum);
+		System.out.println("偶数的和："+evensum);
+		System.out.println("100之内的奇偶数之和："+sum);
 	}
 }
