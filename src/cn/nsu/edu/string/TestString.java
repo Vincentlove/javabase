5f2876d8dbdf25a65476d077eadadef08f04a1c8
Vincent <awende.cool@163.com>
2014-06-03 14:49:23 +0800

    字符串的学习 字符串的拼接

---
+++ src/cn/nsu/edu/string/TestString.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package cn.nsu.edu.string;
+
+/**
+ * @author Vincent
+ * @version 1.0
+ */
+public class TestString {
+	/**
+	 * 用来测试字符串的输出
+	 */
+	public static void string() {
+		String name = " 国家宝藏这个电影我喜欢！";
+		System.out.println(name);
+	}
+
+	/**
+	 * 字符串的拼接
+	 * 
+	 * @param args
+	 */
+	public static void concatenation() {
+		String str1 = "欢迎";
+		String str2 = "xxx";
+		String str3 = "您的到来！";
+
+		System.out.println(str1 + str2 + str3);
+	}
+	/**
+	 * 字符串的另一种写法
+	 */
+	public static void concatenation1() {
+		String str1 = "欢迎";
+		String str2 = str1 + "刘德华";
+		String str3 = str2 + "出场！";
+		System.out.println(str3);
+	}
+	
+	public static void main(String[] args) {
+		// string();
+		concatenation1();
+	}
+}
